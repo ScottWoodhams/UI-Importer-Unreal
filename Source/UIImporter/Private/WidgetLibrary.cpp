ed53f021717b724e4302da1553cd6fc467342b2f
Scott Woodhams <ScottWoodhams@hotmail.com>
2022-02-20 21:24:15 +0000

    Adding widget library

---
+++ Source/UIImporter/Private/WidgetLibrary.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WidgetLibrary.h"
+
+UUserWidget* UWidgetLibrary::GetWidgetFromName(const FString Name)
+{
+	for(FWidgetEntry Entry : Entries)
+	{
+		if(Entry.Name == Name)
+		{
+			return Entry.Widget;
+		}
+	}
+
+	return nullptr;
+}
